e30ec9de2b5a312f2209bf3a8ff55d8f51969783
Cesar Nascimento <cnascimento@dermage.com.br>
2024-02-20 13:12:31 -0300

    fix: Ajuste de ip da API

--- src/utils/myPaths.ts
+++ src/utils/myPaths.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000/";
+const BASE_URL = "http://10.74.51.28:8000/";
 
 export const ME_URL = BASE_URL + "user/me";
 export const LOGIN_URL = BASE_URL + "user/login";
